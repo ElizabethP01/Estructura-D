73d27538bcf5f6e57dccb3316049f9f3353d2345
Eliz <padillachanalexa@gmail.com>
2026-09-08 19:41:30 -0600

    Create M-Dinamica.py

---
+++ M-Dinamica.py
@@ -0,0 +1,15 @@
+frutas = []
+
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uvas")
+
+print(frutas)
+
+frutas.remove("Mango")
+frutas.remove("Banana")
+
+frutas.append("Sandia")
+
+print(frutas)
